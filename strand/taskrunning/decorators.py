389f9fb50b04e71cf53acb9815f9015bf716e3d6
Steve Herzog <steve.herzog@analog.com>
2021-10-21 21:04:35 -0500

    Fix doctests

--- strand/taskrunning/decorators.py
+++ strand/taskrunning/decorators.py
@@ -1,12 +1,13 @@
 """Decorators to simplify taskrunner syntax"""
 
 from functools import wraps
+from typing import Union, Type
 
-from .base import Taskrunner
-from .coroutine import CoroutineTaskrunner
-from .multiprocess import MultiprocessTaskrunner
-from .store_writer import StoreTaskWriter
-from .thread import ThreadTaskrunner
+from strand.taskrunning.base import Taskrunner
+from strand.taskrunning.coroutine import CoroutineTaskrunner
+from strand.taskrunning.multiprocess import MultiprocessTaskrunner
+from strand.taskrunning.store_writer import StoreTaskWriter
+from strand.taskrunning.thread import ThreadTaskrunner
 
 THREAD = 'thread'
 PROCESS = 'process'
@@ -15,7 +16,7 @@ STORE = 'store'
 COROUTINE = 'coroutine'
 
 
-def as_task(target=THREAD, **kwargs):
+def as_task(target: Union[str, Type[Taskrunner]] = THREAD, **kwargs):
     """Creates a decorator to wrap a function in a taskrunner instance.
     When the decorator is called, the taskrunner will be launched
     and the decorated function will be executed within the taskrunner's target context.
@@ -49,23 +50,23 @@ def as_task(target=THREAD, **kwargs):
 
     You can specify a different kind of taskrunner. Please note that different taskrunners
     have different constraints on the kinds of values they can handle (described in those classes).
-    >>> @as_task('process', on_end=end_callback):
+    >>> @as_task('process', on_end=end_callback)
     ... def my_multiprocess_function():
     ...     pass
     ...
-    >>> @as_task('coroutine', yield_on_iter=False):
+    >>> @as_task('coroutine', yield_on_iter=False)
     ... def my_coroutine_function():
     ...     pass
     ...
 
     This example will pickle the function and its arguments when invoked and save them in a store,
     with the assumption that some other process will read from the same store and execute the function later.
-    >>> @as_task('store', store=some_store, pickle_func=True):
+    >>> @as_task('store', store=None, pickle_func=True)
     ... def my_stored_function():
     ...     pass
     """
+
     def deco(func):
-        print(f'target: {target}, kwargs: {dict(kwargs)}')
         if isinstance(target, str):
             if target == THREAD:
                 target_cls = ThreadTaskrunner
@@ -78,7 +79,8 @@ def as_task(target=THREAD, **kwargs):
             elif target == COROUTINE:
                 target_cls = CoroutineTaskrunner
             else:
-                raise ValueError(f'Taskrunner target {target} is invalid. Valid targets are {THREAD}, {PROCESS}, {SYNC}, {STORE}, and {COROUTINE}.')
+                raise ValueError(f'Taskrunner target {target} is invalid. '
+                                 f'Valid targets are {THREAD}, {PROCESS}, {SYNC}, {STORE}, and {COROUTINE}.')
         elif target == Taskrunner or issubclass(target, Taskrunner):
             target_cls = target
         else:
